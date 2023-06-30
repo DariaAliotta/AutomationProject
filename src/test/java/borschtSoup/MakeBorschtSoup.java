0906b80e025308de95a56328e89e2241b3ad8854
DariaAliotta <ishutenkodasha@gmail.com>
2023-06-30 18:39:32 -0400

    firstcomment

---
+++ src/test/java/borschtSoup/MakeBorschtSoup.java
@@ -0,0 +1,47 @@
+package borschtSoup;
+
+import org.testng.Assert;
+
+public class MakeBorschtSoup {
+
+    // Field:
+    String nameX;
+
+    // Method
+    String nameXX(){
+        return "wow";
+    }
+
+    public static void main(String[] args) {
+
+        // Non-Primitive Data Type
+        String name = "Daria";
+        String city = "Brooklyn";
+
+        // Primitive Data types    (8 data types)
+        boolean isHairColorRed = false;
+        char weight = 'a';
+        int luckyNumber = 7;
+        short n1 = 10;
+        long n2 = 2132213;
+        byte n3 = 12;
+        float n4 = 14.25f;
+        double n5 = 14.25;
+
+        System.out.println("Her name is " + name);
+        System.out.println(name + " " + city + " " + n1);
+
+        // ASSERTION:  means compare value between excepted result and actual result
+        String cityName1 = "Brooklyn"; // Excepted result
+        String cityName2 = "Brooklyn"; // Actual result
+
+        Assert.assertEquals(cityName1, cityName2);
+
+
+
+
+
+    }
+
+
+}
