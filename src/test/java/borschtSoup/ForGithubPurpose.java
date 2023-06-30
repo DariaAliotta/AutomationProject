3cccac9e3232ec2054bf8450c96a45e3a98ecc69
DariaAliotta <ishutenkodasha@gmail.com>
2023-06-30 19:01:15 -0400

    hey what is up!Updated

--- src/test/java/borschtSoup/ForGithubPurpose.java
+++ src/test/java/borschtSoup/ForGithubPurpose.java
@@ -3,5 +3,12 @@ package borschtSoup;
 public class ForGithubPurpose {
     public static void main(String[] args) {
 
+        String name ="USA";
+        String lastName = "Russia";
+
+
+
+
+
     }
 }
