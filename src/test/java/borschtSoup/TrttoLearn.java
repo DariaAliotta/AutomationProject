5eb0c8bb8c6faae0147193671a687bffd5a02637
DariaAliotta <ishutenkodasha@gmail.com>
2023-07-11 18:55:10 -0400

    it is second time to push my code into remote repository! please don't help me to push the code foe next time! :)

---
+++ src/test/java/borschtSoup/TrttoLearn.java
@@ -0,0 +1,26 @@
+package borschtSoup;
+
+import java.util.Scanner;
+
+public class TrttoLearn {
+
+    // Write a java program to reverse a String?
+
+
+    public static void main(String[] args) {
+        // It is just practice:
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Hey Daria, can you type any number, it is bigger then zero");
+        String nbr = scan.nextLine();
+
+        System.err.println(" Hey you did good job, I expected this nbr: " + nbr);
+
+
+
+
+
+    }
+
+
+
+}
