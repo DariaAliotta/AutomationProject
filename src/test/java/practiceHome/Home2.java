dec89b35315d7f69781f42113c13d3d3ee594794
DariaAliotta <ishutenkodasha@gmail.com>
2023-08-18 13:51:21 -0400

    Hi, new update

---
+++ src/test/java/practiceHome/Home2.java
@@ -0,0 +1,29 @@
+package practiceHome;
+
+public class Home2 {
+
+    public static void main(String[] args) {
+
+
+        int doorNumber = 7;
+        short a = 2 ;
+        long b =14324;
+        byte c = 8;
+        float d = 8.27f;
+        double e = 72.88;
+        char favoriteSeatInCinema ='1';
+        boolean isSunsetAt8pmToday = true;
+        String howManyDataTypes = "9";
+
+        System.out.println (doorNumber);
+        System.out.println (a);
+        System.out.println (b);
+        System.out.println (c);
+        System.out.println (d);
+        System.out.println (e);
+        System.out.println (favoriteSeatInCinema);
+        System.out.println (isSunsetAt8pmToday);
+        System.out.println (howManyDataTypes);
+
+    }
+}
